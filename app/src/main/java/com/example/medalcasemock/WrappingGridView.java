18832bf1d1cf64efd464a181b68b2fddeec78424
Jerry <jerry@Jerrys-MBP.hitronhub.home>
2021-08-21 11:41:14 -0700

    grid view scroll working

---
+++ app/src/main/java/com/example/medalcasemock/WrappingGridView.java
@@ -0,0 +1,31 @@
+package com.example.medalcasemock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.GridView;
+
+public class WrappingGridView extends GridView {
+
+    public WrappingGridView(Context context) {
+        super(context);
+    }
+
+    public WrappingGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WrappingGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightSpec = heightMeasureSpec;
+        if (getLayoutParams().height == LayoutParams.WRAP_CONTENT) {
+            heightSpec = View.MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        }
+        super.onMeasure(widthMeasureSpec, heightSpec);
+    }
+
+}
